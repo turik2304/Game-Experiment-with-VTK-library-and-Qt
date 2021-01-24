093f780ae1334fd64f05f577e748d973c15802e0
Artur <73557404+turik2304@users.noreply.github.com>
2021-01-24 21:08:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
     setlocale(LC_NUMERIC,"C");
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
-    // Create and start Lab#, #=0..3
     GameWindow w;
     w.show();
     return a.exec();
